532d446bca6d1c672e7d664b6177fec68cefda18
Melis Nur Verir <melisnurverir@gmail.com>
2022-01-05 15:01:27 +0300

    adds tours project

---
+++ tours/src/App.js
@@ -0,0 +1,13 @@
+import logo from './images/logo.png';
+
+function App() {
+  return (
+    <>
+      <header>
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </>
+  );
+}
+
+export default App;
