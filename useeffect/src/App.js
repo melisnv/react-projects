182b9fecd7fd4ae87fe4735a54390b89c59ef19a
Melis Nur Verir <melisnurverir@gmail.com>
2022-01-05 14:03:06 +0300

    adds show hide component

--- useeffect/src/App.js
+++ useeffect/src/App.js
@@ -3,11 +3,12 @@ import UseEffectBasics from './useeffect-basics';
 import UseEffectCleanUp from './useeffect-cleanup';
 import UseEffectFetchData from './github-users-fetchingdata';
 import UseEffectConditionalRendering from './useeffect-conditional-rendering';
+import ShowHideComponent from './showhide-component';
 
 function App() {
   return (
     <div className="container">
-      <UseEffectFetchData />
+      <ShowHideComponent />
     </div>
   );
 }
