58b56ec206fee1f392af9bfbb72c43ad2230dfd5
melisnv <melisnurverir@gmail.com>
2022-03-20 10:29:29 +0300

    adds cart component

--- cart-component/src/context.js
+++ cart-component/src/context.js
@@ -1,3 +1,4 @@
+import { type } from '@testing-library/user-event/dist/type';
 import React, { useState, useContext, useReducer, useEffect } from 'react';
 import cartItems from './data';
 import reducer from './reducer';
@@ -11,10 +12,34 @@ const initialState = { loading: false, cart: cartItems, total: 0, amount: 0 };
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  const clearCart = () => {
+    dispatch({ type: 'CLEAR_CART' });
+  };
+
+  const removeItem = (id) => {
+    dispatch({ type: 'REMOVE_ITEM', payload: id });
+  };
+
+  const increase = (id) => {
+    dispatch({ type: 'INCREASE', payload: id });
+  };
+
+  const decrease = (id) => {
+    dispatch({ type: 'DECREASE', payload: id });
+  };
+
+  useEffect(() => {
+    dispatch({ type: 'GET_TOTALS' });
+  }, [state.cart]);
+
   return (
     <AppContext.Provider
       value={{
         ...state,
+        clearCart, // so that whole app can access this function
+        removeItem,
+        increase,
+        decrease,
       }}
     >
       {children}
