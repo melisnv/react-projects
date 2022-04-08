ea2a8403f8d402bd1879bd6cee05328d0509b214
melisnv <melisnurverir@gmail.com>
2022-04-08 20:25:04 +0300

    adds meals component

--- food-hunt/src/SearchMenu/components/Meals.js
+++ food-hunt/src/SearchMenu/components/Meals.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Meals = () => {
-  return <h3>Meals Component</h3>;
+const Meals = ({ id, name, image, info, origin }) => {
+  return (
+    <article className="meals">
+      <div className="meals__image">
+        <img src={image} alt={name} />
+        <div className="meals__footer">
+          <h3>{name}</h3>
+          <h4>{origin}</h4>
+          <p>{info}</p>
+        </div>
+      </div>
+    </article>
+  );
 };
 
 export default Meals;
