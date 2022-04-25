2f4594bc38d38da21b4044fc6e55ee8939d96667
melisnv <melisnurverir@gmail.com>
2022-04-25 08:27:15 +0300

    adds popular component

--- cosmetic-website/src/components/Popular.js
+++ cosmetic-website/src/components/Popular.js
@@ -1,20 +1,35 @@
 import React from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import styledComponents from 'styled-components';
 
 const url =
   'http://makeup-api.herokuapp.com/api/v1/products.json?brand=covergirl&product_type=lipstick';
 
 const Popular = () => {
+  const [popular, setPopular] = useState([]);
+
   const getData = async () => {
     const response = await fetch(url);
     const data = await response.json();
+    console.log(data);
+    setPopular(data);
   };
 
   useEffect(() => {
     getData();
   }, []);
 
-  return <div>Popular</div>;
+  return (
+    <div>
+      {popular.map((item, id) => {
+        return (
+          <article key={id}>
+            <h3>{item.name}</h3>
+          </article>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Popular;
