b87d74b38f52ff9262291acf1a6898068ea7c211
melisnv <melisnurverir@gmail.com>
2022-05-15 11:32:13 +0300

    changes logo

--- travel-agency/src/components/Navbar.js
+++ travel-agency/src/components/Navbar.js
@@ -36,7 +36,7 @@ const Navbar = () => {
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-            LOGO
+            TRVL
             <FaPlaneDeparture className="fa-plane" />
           </Link>
 
