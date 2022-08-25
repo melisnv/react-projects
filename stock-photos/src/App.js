79054cb46df5dfc4e719f8f604094cfc89b931d5
melisnv <melisnurverir@gmail.com>
2022-08-25 23:38:16 +0300

    adds infinite scrolling and search component

--- stock-photos/src/App.js
+++ stock-photos/src/App.js
@@ -10,15 +10,29 @@ const searchURL = 'https://api.unsplash.com/search/photos/';
 function App() {
   const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState('');
 
   const fetchImages = async () => {
     setLoading(true);
     let url;
-    url = `${mainURL}${clientID}`;
+    const urlPage = `&page=${page}`;
+    const urlQuery = `&query=${query}`;
+    if (query) {
+      let url = `${searchURL}${clientID}${urlPage}${urlQuery}`;
+    } else {
+      url = `${mainURL}${clientID}${urlPage}`;
+    }
     try {
       const response = await fetch(url);
       const data = await response.json();
-      setPhotos(data);
+      setPhotos((oldPhotos) => {
+        if (query) {
+          return [...oldPhotos, ...data.results];
+        } else {
+          return [...oldPhotos, ...data];
+        }
+      });
       setLoading(false);
     } catch (error) {
       setLoading(false);
@@ -28,17 +42,38 @@ function App() {
 
   useEffect(() => {
     fetchImages();
+  }, [page]);
+
+  useEffect(() => {
+    const event = window.addEventListener('scroll', () => {
+      if (
+        !loading &&
+        window.innerHeight + window.scrollY >= document.body.scrollHeight - 2
+      ) {
+        setPage((oldPage) => {
+          return oldPage + 1;
+        });
+      }
+    });
+    return () => window.removeEventListener('scroll', event);
   }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    fetchImages();
   };
 
   return (
     <main>
       <section className="search">
         <form className=" search-form">
-          <input type="text" placeholder="search" className="form-input" />
+          <input
+            type="text"
+            placeholder="search"
+            className="form-input"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
           <button type="submit" className="submit-btn" onClick={handleSubmit}>
             <FaSearch />
           </button>
@@ -46,7 +81,7 @@ function App() {
       </section>
       <section className="photos">
         <div className="photos-center">
-          {photos.map((image, index) => {
+          {photos.map((image, id) => {
             return <Photo key={image.id} {...image} />;
           })}
         </div>
