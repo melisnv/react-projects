c853db563466bcadac32d3495d48c68019d2403b
melisnv <melisnurverir@gmail.com>
2023-12-25 15:01:55 +0100

    structuring the project

---
+++ travel-inn-app/src/components/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Hero = () => {
+  return (
+    <section className="hero" id="home">
+      <div className="hero-banner">
+        <h1>Journey Beyond Boundaries</h1>
+        <p>Indulge in Luxury Escapes Designed Just for You</p>
+        <a href="#tours" className="btn hero-btn">
+          explore tours
+        </a>
+      </div>
+    </section>
+  );
+}
